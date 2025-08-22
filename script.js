2fcfadcb497ff7f3141d3cdfd2e893c25c9b0191
LexhollowGit <lexhollowi@gmail.com>
2025-08-22 13:34:27 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+// Resource
+let energy = 0;
+
+// Get elements
+const energyDisplay = document.getElementById("energy");
+const collectBtn = document.getElementById("collect");
+
+// Collect energy when clicking
+collectBtn.addEventListener("click", () => {
+  energy += 1;
+  updateDisplay();
+  saveGame();
+});
+
+// Update resource display
+function updateDisplay() {
+  energyDisplay.textContent = energy;
+}
+
+// Save to localStorage
+function saveGame() {
+  localStorage.setItem("cosmicLabsSave", JSON.stringify({ energy }));
+}
+
+// Load saved game
+function loadGame() {
+  const save = JSON.parse(localStorage.getItem("cosmicLabsSave"));
+  if (save) {
+    energy = save.energy || 0;
+  }
+  updateDisplay();
+}
+
+loadGame();
